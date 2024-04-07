f7e7cf26b491c2738cfa8f669b4840118fabbc56
MoriFGC <abdulmoha@outlook.it>
2024-04-07 15:32:41 +0200

    extra

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,29 @@
-let film  = document.querySelectorAll(".scroll")
+// faccio in modo che quando scrollo sul documento succedono cose...
+document.addEventListener('scroll', function() {
+  // vado a puntare la classe scroll che ho messo negli ultimi 3 "caroselli"
+  let films = document.querySelectorAll('.scroll');
+// queste sono le opzioni che ho per visualizzare l'elemento
+  let options = {
+    root: null, // null sta per viewport
+    rootMargin: '0px', // questo è il margine che serve per visualizzare l'elemento
+    threshold: 0.1 // appena visualizzo il 10% dell'elemento compare
+  };
 
-document.addEventListener("scroll", function() {
-    film.forEach(carosello => {
-        carosello.classList.add("show-carousel")
+  // la funzione di callback che verrà eseguita quando un determinato elemento entra o esce dalla viewport.
+  function callback(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // se l'elemento entra nella viwport allora aggiungi la classe
+        entry.target.classList.add('show-carousel');
+      } else {
+        entry.target.classList.remove('show-carousel');
+      }
     });
-})
+  }
+
+  let observer = new IntersectionObserver(callback, options);
+ // questo è l'elemento che voglio osservare 
+  films.forEach(film => {
+    observer.observe(film);
+  });
+});
